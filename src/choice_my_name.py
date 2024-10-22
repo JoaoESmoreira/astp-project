0522b78b4e17343f9434567e15865cdc9721cabb
JoaoESmoreira <jesmoreira2010@gmail.com>
2024-10-22 13:48:26 +0100

    fix and add Plotify

--- src/choice_my_name.py
+++ src/choice_my_name.py
@@ -20,66 +20,75 @@ class Problem():
     def empty_temperature_assement(self):
         return AssByDiff(np.array(self.df['temperature_mean']))
 
+class Plotify():
+    def __init__(self):
+        sns.set_style("darkgrid")
+
+    def plot(self, xl, yl, titles, xlabel="", ylabel="", trend=False):
+        _, axes = plt.subplots(len(titles), figsize=(12, 6))
+        for i in range(len(titles)):
+            x = xl[i]
+            y = yl[i]
+            sns.lineplot(ax=axes[i], x=x, y=y)
+
+            if trend:
+                coefficients = np.polyfit(x, y, 1)
+                p = np.poly1d(coefficients)
+                sns.lineplot(ax=axes[i], x=x, y=p(x), lw=1.2, label="Trend") # color="darkorange", linestyle='--', 
+
+            axes[i].set_xlabel(xlabel)
+            axes[i].set_ylabel(ylabel)
+            axes[i].set_title(titles[i])
+        plt.tight_layout()
+        plt.show()
+
+    def scatter(self, xl, yl, titles, xlabel="", ylabel=""):
+        _, axes = plt.subplots(len(titles), figsize=(12, 6))
+        for i in range(len(titles)):
+            x = xl[i]
+            y = yl[i]
+            sns.scatterplot(ax=axes[i], x=x, y=y)
+
+            axes[i].set_xlabel(xlabel)
+            axes[i].set_ylabel(ylabel)
+            axes[i].set_title(titles[i])
+        plt.tight_layout()
+        plt.show()
+
 class AssByDiff():
     def __init__(self, ts: np.ndarray):
         self.original_ts = ts
         self.dfs = []
+        self.f = []
+        self.m = []
         self.titles = []
+        self.plotter = Plotify()
 
     def differentiation(self, f: int):
-        self.titles = ['Original', 'Original - mean', 'Delta(n)', 'Delta_12(Delta(n))']
+        self.titles = ['Original',  'Delta(n)', 'Delta_12(Delta(n))']
         original_median = self.original_ts - self.original_ts.mean()
         delta = np.diff(self.original_ts)
         delta_f = np.diff(delta, n=f)
-        self.dfs = [self.original_ts, original_median, delta, delta_f]
+        self.dfs = [original_median, delta, delta_f]
 
-    def plotfy_fft(self, t: float):
-        sns.set_style("darkgrid")
-        _, axes = plt.subplots(len(self.titles), figsize=(12, 6), sharex=True)
+    def fft(self, t: float):
         for i in range(len(self.titles)):
             signal = self.dfs[i]
-
             fft = np.fft.fft(signal) / signal.shape[0]
             magnitude = np.abs(fft)
             frequencies = np.fft.fftfreq(len(signal), t)
+            self.f.append(frequencies[:len(frequencies)//2])
+            self.m.append(magnitude[:len(magnitude)//2])
 
-            sns.lineplot(ax=axes[i], x=frequencies[:len(frequencies)//2], y=magnitude[:len(magnitude)//2])
-            axes[i].set_xlabel("Frequency (In years)")
-            axes[i].set_ylabel("Magnitude (Hz)")
-            axes[i].set_title(self.titles[i])
-        plt.tight_layout()
-        plt.show()
+    def plotfy_fft(self):
+        self.plotter.plot(self.f, self.m, self.titles, "Frequency (In years)", "Magnitude (Hz)")
 
     def plotfy_diff(self, n: int):
-        sns.set_style("darkgrid")
-        _, axes = plt.subplots(len(self.titles), figsize=(12, 6), sharex=True)
-        for i in range(len(self.titles)):
-            signal = self.dfs[i][:n]
+        y = [self.dfs[i][:n] for i in range(3)]
+        x = [np.arange(y[i].shape[0]) for i in range(3)]
 
-            # Calcular a tendência
-            x = np.arange(signal.shape[0])
-            coefficients = np.polyfit(x, signal, 1)
-            p = np.poly1d(coefficients)
+        self.plotter.plot(x, y, self.titles, "Time", "Mean Temperature (Cº)", trend=True)
 
-            sns.lineplot(ax=axes[i], data=signal, lw=1, label="Signal") #, color="royalblue")
-            sns.lineplot(ax=axes[i], x=x, y=p(x), lw=1.2, label="Trend") # color="darkorange", linestyle='--', 
-            axes[i].set_xlabel("Time")
-            axes[i].set_ylabel("Mean Temperature (Cº)")
-            axes[i].set_title(self.titles[i])
-        plt.tight_layout()
-        plt.show()
-
-    def plotfy(self):
-        plt.figure(figsize=(14, 6))
-
-        plt.plot(self.original_ts)
-
-        plt.title("Time Series of Temperature Mean")
-        plt.xlabel("Time")
-        plt.ylabel("Temperature Mean (ºC)")
-        plt.legend(["Training Dataset", "Testing Dataset", "Trend"])
-        plt.show()
-    
     def create_correlogram(self):
         return Correlogram(self.dfs, self.titles)
 
@@ -87,18 +96,9 @@ class Correlogram():
     def __init__(self, lTS: list[np.ndarray], titles: list[str]):
         self.lTS = lTS # list of time series
         self.titles = titles
-
-    # def _pearson_corr(self, x: np.ndarray, y: np.ndarray=None):
-    #     x_mean = x.mean()
-    #     y_mean = y.mean()
-    #     numinator = (x-x_mean) * (y-y_mean)
-    #     numinator = numinator.sum()
-    #     temp_1 = (x-x_mean)**2
-    #     temp_1 = temp_1.sum()
-    #     temp_2 = (y-y_mean)**2
-    #     temp_2 = temp_2.sum()
-    #     denuminator = np.sqrt(temp_1 * temp_2)
-    #     return numinator / denuminator
+        self.x = []
+        self.corr = []
+        self.plotter = Plotify()
 
     def correlation(self, values, n):
         df = pd.DataFrame(data=values, columns=['values'])
@@ -108,17 +108,14 @@ class Correlogram():
         return corr
 
     def correlogram(self, n):
-        _, axes = plt.subplots(len(self.titles), figsize=(8, 6))
-
         for i in range(len(self.lTS)):
             signal = self.lTS[i]
             corr = self.correlation(signal, n)
-            x = np.arange(len(corr))
+            self.corr.append(corr)
+            self.x.append(np.arange(len(corr)))
 
-            sns.scatterplot(ax=axes[i], x=x, y=corr)
-            axes[i].set_ylabel(self.titles[i])
-
-        plt.show()
+    def plotify(self, n):
+        self.plotter.scatter(self.x, self.corr, self.titles, "Lag", "Autocorrelation")
 
     def stats_tests(self):
         for i in range(len(self.titles)):
@@ -139,9 +136,11 @@ if __name__ == "__main__":
 
     s = p.empty_temperature_assement()
     s.differentiation(f=365)
-    s.plotfy_fft(t=1/365)
+    s.fft(t=1/365)
+    s.plotfy_fft()
     s.plotfy_diff(n=600)
 
     c = s.create_correlogram()
-    # c.correlogram(365)
+    c.correlogram(365)
+    c.plotify(365)
     # c.stats_tests()
